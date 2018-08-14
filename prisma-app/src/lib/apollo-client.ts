3d1803e16143a386ec490a3c7a1ad391b9dfb6fa
kazu <kazu8160@gmail.com>
2018-08-14 23:05:30 +0900

    Subscription追加のためapollo-clientの実装を変更

--- prisma-app/src/lib/apollo-client.ts
+++ prisma-app/src/lib/apollo-client.ts
@@ -1,14 +1,37 @@
 import ApolloCLient from 'apollo-client'
+import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+// see: https://github.com/apollographql/apollo-client/issues/3090
+interface IDefinintion {
+  kind: string;
+  operation?: string;
+};
+
+const wsLink = new WebSocketLink({
+  options: { reconnect: true },
+  uri: 'ws://localhost:4466',
+})
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4466',
 })
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation }: IDefinintion = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
 export default function apolloClient() {
   return new ApolloCLient({
     cache: new InMemoryCache(),
-    link: httpLink,
+    link,
   });
 }
